451088a366243d886fc0d9c7bfce3a82cc30ff0f
Junior Rojas <rojasjuniore@gmail.com>
2020-09-24 17:59:52 +0400

    The Complete Guide

---
+++ routes/api/auth.js
@@ -0,0 +1,27 @@
+const router = require("express").Router();
+
+// Controllers
+const {
+    register,
+    verify,
+    login,
+    resendVerification,
+    getAuthenticatedUser
+} = require("../../app/controllers/api/AuthController");
+
+
+// Middleware
+const { registerValidation, loginValidation, auth } = require("../../app/middlewares/auth");
+
+// Routes
+router.post("/register", registerValidation, register);
+router.get("/verify/:token", verify);
+router.post("/verify/resend", resendVerification);
+router.post("/login", loginValidation, login);
+
+router.get("/", auth, getAuthenticatedUser);
+
+
+
+
+module.exports = router;
